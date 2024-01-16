9547a71cf3a39daa82a490ceae9999465ef9fd54
kiran <kiranbchanni@gmail.com>
2024-01-16 23:58:03 +0530

    File modified

--- if-cond/content-check-in-file.sh
+++ if-cond/content-check-in-file.sh
@@ -8,6 +8,8 @@ out=$(grep $cont $pat |wc -l)
 if [[ $out > 0 ]]
 then
 echo "Content is present"
+echo "Below are the lines where content $cont exits in file $pat"
+grep -n $cont $pat       ====================>> **
 else
 echo "Content is not present"
 fi
@@ -47,3 +49,28 @@ echo "Content $cont not present"
 else
 echo "Invalid input"
 fi
+
+=================================belo script is not work==============================
+#!/bin/bash
+echo "Enyter the content to be checked"
+read cont
+
+for i in $(cat kiran.txt)
+do
+if [[ $i == $cont ]]
+then
+echo "Content is present in file"
+else
+echo "Content  not present in file"
+fi
+done
+
+output is 
+Enyter the content to be checked
+kiran
+Content is present in file
+Content  not present in file
+
+it showing both present and not present because i is check each line for 1 line kiran present 
+and 2nd line kiran not present 
+==============================================================================================
---
+++ while-loop/maths.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+while true
+do
+
+echo -e "Enter 1 for Addition\nEnter 2 for Substraction\nEnter 3 for Multiplication\nEnter 4 for Division"
+read opt
+if [ $opt -eq 1 ]
+then
+echo "Enter 1st number"
+read num1
+echo "Enter 2nd number"
+read num2
+sum=$(($num1+$num2))
+echo "Addition of number $num1 and $num2 is $sum"
+elif [ $opt -eq 2 ]
+then
+echo "Enter 1st number"
+read num1
+echo "Enter 2nd number"
+read num2
+sub=$(($num1-$num2))
+echo "Substraction of number $num1 and $num2 is $sub"
+elif [ $opt -eq 3 ]
+then
+echo "Enter 1st number"
+read num1
+echo "Enter 2nd number"
+read num2
+mult=$(($num1*$num2))
+echo "Multiplication of number $num1 and $num2 is $mult"
+elif [ $opt -eq 4 ]
+then
+echo "Enter 1st number"
+read num1
+echo "Enter 2nd number"
+read num2
+while [ $num2 = 0 ];
+do
+echo "Enter 2nd number"
+read num2
+divi=$(($num1/$num2))
+done
+fi
+
+# if [[ $num2 -eq 0 ]]
+# then
+# echo "Entered number is 0, Enter the number greater than 0"
+# echo "Enter 1st number"
+# read k
+# echo "Enter 2nd number"
+# read b
+# d=$(($k/$b))
+# echo "Division of number $k and $b is $d"
+
+# elif [[ $num2 -gt 0 ]]
+# then
+# divi=$(($num1/$num2))
+# echo "Division of number $num1 and $num2 is $divi"
+# fi
+# else
+# echo "Entered wrong option"
+# fi
+
+sleep 2
+done
